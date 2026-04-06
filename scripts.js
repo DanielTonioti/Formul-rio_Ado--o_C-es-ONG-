74fba15f27e6a58b45468be3f6b2b74d5d384741
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-04-06 20:27:07 -0300

    add- coloquei listeners dinamicos, para melhor experiencia e cumprir a parte da morada

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
+// Essa parte aqui serve pra fz com q os campos aparecam ou desapareçam
+const Morada = document.getElementById("Morada");
+const DivPermissao = document.getElementById("DivPermissao");
+const DivSeguranca = document.getElementById("DivSeguranca");
+const QuintalSim = document.getElementById("QuintalSIm");
+const QuintalNao = document.getElementById("QuintalNao");
 
+function mostrarPermissao(){
+    if(Morada.value === "Ap"){
+        DivPermissao.classList.remove("hidden");
+    } else {
+        DivPermissao.classList.add("hidden");
+    }
+    if(Morada.value === "Casa" && QuintalSim.checked){
+        DivSeguranca.classList.remove("hidden");
+    } else {
+        DivSeguranca.classList.add("hidden");
+    }
+}
+Morada.addEventListener("change", mostrarPermissao);
+QuintalSim.addEventListener("change", mostrarPermissao);
+QuintalNao.addEventListener("change", mostrarPermissao);
+
+//Parte que o Icaro passou em sala
 document.getElementById("FormAdocaoCaes").addEventListener("submit", function (e){
 e.preventDefault();
-
+let Idade = document.getElementById("Idade").value;
 });
